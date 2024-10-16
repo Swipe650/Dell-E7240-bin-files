a87e45e72df641597abcb8883de3f908dc8facf1
Swipe650 <swipe650@gmail.com>
2024-10-16 16:03:29 +0100

    initial commit

---
+++ mute_wiim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+vol=$(playerctl -p wiimplay volume | cut -c4-5 | bc -l)
+
+    if [[ "$vol" -eq "00" ]]; then
+      playerctl -p wiimplay volume 0.43
+      
+      
+    elif [[ "$vol" -gt "01" ]]; then
+      playerctl -p wiimplay volume 0
+    fi 
