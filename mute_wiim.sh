d1849c5e31a48d903664c7ead81887bbccad41a0
Swipe650 <swipe650@gmail.com>
2024-10-31 09:15:11 +0000

    changed to -c3-4

--- mute_wiim.sh
+++ mute_wiim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-vol=$(playerctl -p wiimplay volume | cut -c4-5 | bc -l)
+vol=$(playerctl -p wiimplay volume | cut -c3-4 | bc -l)
 
     if [[ "$vol" -eq "00" ]]; then
-      playerctl -p wiimplay volume 0.29
+      playerctl -p wiimplay volume 0.27
       
       
     elif [[ "$vol" -gt "01" ]]; then
