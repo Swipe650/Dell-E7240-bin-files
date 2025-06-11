0aa6477207539daddc106d96065f1228f131e9b4
Swipe650 <swipe650@gmail.com>
2025-06-11 18:10:32 +0100

    updated

---
+++ mute_livingroom_mini.sh
@@ -0,0 +1,14 @@
+sleep-with-countdown() {
+  cast-linux-amd64 --name "Living Room mini" volume 0
+  secs=$1
+  while [ $secs -gt 0 ]; do
+    printf "\rsleep: $secs\033[0K"
+    sleep 1
+    : $((secs--))
+  done
+  printf "\n"
+  cast-linux-amd64 --name "Living Room mini" volume 0.4
+}
+
+sleep-with-countdown $1
+
