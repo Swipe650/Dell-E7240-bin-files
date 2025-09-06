6404e0fd25403205b267f21092a740ea64b4f321
Swipe650 <swipe650@gmail.com>
2025-09-06 12:37:51 +0100

    updated to support mute/unmute command on google homes

--- mute_home.sh
+++ mute_home.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#google_home="Kitchen home"
+google_home="Kitchen home"
 #google_home="Bedroom mini"
-#google_home="Multiroom mini"
-google_home="Living Room mini"
 
-vol=$(/home/swipe/bin/cast-linux-amd64 --name "$google_home" status | awk -F 'Volume:' '{print $2}' | cut -c4-5 | bc -l)
+# Get the volume status
+vol=$(/home/swipe/bin/cast-linux-amd64 --name "$google_home" status | awk -F 'Volume:' '{print $2}' | awk '/muted/ { print "true"; exit }')
 
-    if [[ "$vol" -eq "00" ]]; then
-      /home/swipe/bin/cast-linux-amd64 --name "$google_home" volume .31
-      
-      
-    elif [[ "$vol" -gt "01" ]]; then
-      /home/swipe/bin/cast-linux-amd64 --name "$google_home" volume 0
-    fi 
+# Check if it's muted (vol will be "true" if muted, empty if not)
+if [ "$vol" == "true" ]; then
+    # Unmute the device
+    /home/swipe/bin/cast-linux-amd64 --name "$google_home" unmute
+elif [ -z "$vol" ]; then
+    # Mute the device if it's not already muted
+    /home/swipe/bin/cast-linux-amd64 --name "$google_home" mute
+fi
